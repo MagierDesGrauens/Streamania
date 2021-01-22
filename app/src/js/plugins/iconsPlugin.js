fb74dfe052077839c110c6b97aeaec2560a19b64
DerGamer22 <jeromelemoine12@googlemail.com>
2021-01-22 12:11:34 +0100

    classes will be maintained when changing <i> to <svg>

--- app/src/js/plugins/iconsPlugin.js
+++ app/src/js/plugins/iconsPlugin.js
@@ -1,14 +1,19 @@
-let elems = document.querySelectorAll('[class^="bi-"]');
+let elems = document.querySelectorAll('[class*="bi-"]');
 
 elems.forEach(elem => {
+    let otherClasses = "";
     elem.classList.forEach(classes => {
         if (classes.startsWith('bi-')) {
             iconClass = classes.split('-')[1];
         }
+        else {
+            otherClasses += " " + classes;
+        }
     });
+
     $(elem).replaceWith(`
-    <svg class="bi" width="32" height="32" fill="currentColor">
-        <use xlink:href="svg/bootstrap-icons.svg#${iconClass}"/>
-    </svg>
+        <svg class="bi${otherClasses}" width="32" height="32" fill="currentColor">
+            <use xlink:href="svg/bootstrap-icons.svg#${iconClass}"/>
+        </svg>
     `);
 });
