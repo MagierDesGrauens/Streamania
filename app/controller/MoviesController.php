508f6a77fe58ec1ea48583b1e4753af357b536ba
MagierDesGrauens <klebedose95@web.de>
2021-02-05 15:07:53 +0100

    Add demodata to installer for movies and series, implement movies and series

--- app/controller/MoviesController.php
+++ app/controller/MoviesController.php
@@ -1,10 +1,12 @@
 <?php
 
 use \Streamania\Controller;
+use \Streamania\Database;
 
 class MoviesController extends Controller
 {
     public function IndexAction()
     {
+        $this->model->movies = Database::fetch('SELECT * FROM videos WHERE type="movie"');
     }
 }
--- app/controller/SeriesController.php
+++ app/controller/SeriesController.php
@@ -1,10 +1,12 @@
 <?php
 
 use \Streamania\Controller;
+use \Streamania\Database;
 
 class SeriesController extends Controller
 {
     public function IndexAction()
     {
+        $this->model->series = Database::fetch('SELECT * FROM videos WHERE type="series"');
     }
 }
